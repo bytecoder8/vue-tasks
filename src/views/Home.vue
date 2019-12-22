a8ac220b6b0c6c710199643c57ce1f283efe0dd8
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2019-12-22 23:43:01 +0500

    Create and view task list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,91 @@
 <template>
-  <div>
-    <h1>Create</h1>
+  <div class="row">
+    <form class="col s6 offset-s3" @submit.prevent="handleSubmit" method="post">
+      <h1>Create</h1>
+      <div class="input-field">
+        <input type="text" v-model="title" name="title" id="title" class="validate" required>
+        <label for="title">Title</label>
+        <span class="helper-text" data-error="Title is required"></span>
+      </div>
+      <div class="chips" ref="chips"></div>
+      <div class="input-field">
+        <textarea
+          v-model="description"
+          name="description" 
+          id="description"
+          class="materialize-textarea validate"
+          :maxlength="maxDescLength"
+          required
+        ></textarea>
+        <label for="description">Description</label>
+        <span class="character-counter">{{ descLength }}/{{ maxDescLength }}</span>
+      </div>
+      <div class="input-field">
+        <input type="text" ref="datepicker" class="datepicker">
+      </div>
+      <div class="input-field">
+        <button type="submit" class="btn waves-effect waves-light">Send</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<script>
+<style scoped>
+  .character-counter {
+    float: right;
+  }
+</style>
 
+<script>
 export default {
   name: 'home',
-  components: {
+  mounted() {
+    this.chips = window.M.Chips.init(this.$refs.chips, {
+      placeholder: 'Tags'
+    })
+
+    this.date = window.M.Datepicker.init(this.$refs.datepicker, {
+      format: 'dd.mm.yyyy',
+      defaultDate: new Date(),
+      setDefaultDate: true
+    })
+  },
+  destroyed() {
+    if (this.date && this.date.destroy) {
+      this.date.destroy()
+    }
+    if (this.chips && this.chips.destroy) {
+      this.chips.destroy()
+    }
+  },
+  data() {
+    return {
+      title: '',
+      description: '',
+      chips: null,
+      date: null,
+      maxDescLength: 2048
+    }
+  },
+  computed: {
+    descLength() {
+      return this.description.length
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const task = {
+        id: Date.now(),
+        title: this.title,
+        description: this.description,
+        tags: this.chips.chipsData,
+        date: this.date.date,
+        status: 'active'
+      }
+
+      this.$store.dispatch('createTask', task)
+      this.$router.push('/list')
+    }
   }
 }
 </script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
+  <div class="row">
     <h1>Task List</h1>
+    <table v-if="tasks.length">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) of tasks" :key="task.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ new Date(task.date).toLocaleDateString() }}</td>
+          <td>{{ task.title }}</td>
+          <td class="desc-td"><div class="desc-text">{{ task.description }}</div></td>
+          <td>{{ task.status }}</td>
+          <td>
+            <router-link 
+              tag="button" class="btn bnt-small waves-effect waves-light" 
+              :to="'/task/' + task.id"
+            >Open</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>
+      No tasks yet. <router-link to="/">Create new one?</router-link>
+    </p>
   </div>
 </template>
+
+<style scoped>
+  .desc-td {
+    max-width: 350px;
+  }
+  .desc-text {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+</style>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['tasks'])
+  }
+}
+</script>
