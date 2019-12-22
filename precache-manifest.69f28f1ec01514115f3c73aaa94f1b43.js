3e5ee170d58ffc5b8293889e4c8d2faa673e8415
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2019-12-23 01:56:15 +0500

    Add files via upload

---
+++ precache-manifest.69f28f1ec01514115f3c73aaa94f1b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1ce1bb13bb2f009064",
+    "url": "/vue-tasks/css/app.ca103074.css"
+  },
+  {
+    "revision": "91021be984e5772b6314",
+    "url": "/vue-tasks/css/chunk-vendors.2955f2db.css"
+  },
+  {
+    "revision": "b92926a9f75503811b16486dad9edab1",
+    "url": "/vue-tasks/index.html"
+  },
+  {
+    "revision": "2d1ce1bb13bb2f009064",
+    "url": "/vue-tasks/js/app.3406bccf.js"
+  },
+  {
+    "revision": "91021be984e5772b6314",
+    "url": "/vue-tasks/js/chunk-vendors.afb908db.js"
+  },
+  {
+    "revision": "c70241f05f3e1e4d1084f3dfb131d8fc",
+    "url": "/vue-tasks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-tasks/robots.txt"
+  }
+]);
